6c6f5a05ee3e519faa05b9897d90459ab92cfb9d
kishorkunal21 <kishorkunal21@gmail.com>
2025-06-17 07:43:08 +0530

    CustomCollector - wip

---
+++ src/com/makemyown/CustomCollector.java
@@ -0,0 +1,36 @@
+package com.makemyown;
+
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+//todo : to implement and check
+public class CustomCollector implements Collector {
+    @Override
+    public Supplier supplier() {
+        return null;
+    }
+
+    @Override
+    public BiConsumer accumulator() {
+        return null;
+    }
+
+    @Override
+    public BinaryOperator combiner() {
+        return null;
+    }
+
+    @Override
+    public Function finisher() {
+        return null;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Set.of();
+    }
+}
