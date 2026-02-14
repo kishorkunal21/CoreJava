926ce1a5d74f3d489453e8003afd631c4167facd
kishorkunal21 <kishorkunal21@gmail.com>
2026-02-14 23:00:42 +0530

    NonConstructableChange - AlgoExpert

--- src/com/algoexpert/arrays/NonConstructableChange.java
+++ src/com/algoexpert/arrays/NonConstructableChange.java
@@ -13,33 +13,33 @@ public class NonConstructableChange {
 
     public static void main(String[] args) {
         //nonConstructableChange(new int[]{5, 7, 1, 1, 2, 3, 22});
-        nonConstructableChange(new int[]{6, 4, 5, 1, 1, 8, 9});
-
-
+        //nonConstructableChange(new int[]{6, 4, 5, 1, 1, 8, 9});
+        //nonConstructableChange(new int[]{4, 2, 1 });
+        //nonConstructableChange(new int[]{1, 2, 5});
+        //nonConstructableChange(new int[]{5, 7, 1, 1, 2, 3, 22});
+        nonConstructableChange(new int[]{1, 1, 2, 3, 4, 5, 6, 9});
     }
 
-    static int nonConstructableChange(int[] input){
-        int min = 1;
-
-        if(input.length>1){
-            List<Integer> inputList = Arrays.stream(input).boxed().collect(Collectors.toList());
-            inputList = inputList.stream().sorted().collect(Collectors.toList());
-            inputList.stream().forEach(System.out::println);
+    static int nonConstructableChange(int[] coins) {
+        int min = 0;
+        // logic : if the new entry is > sum +1 ; change is not possible
+        List<Integer> minList = new ArrayList<>();
 
 
-            for(int i=0;i<inputList.get(inputList.size()-1);i++){
-                min = min+1;
-                if( (inputList.get(i)+inputList.get(i+1))==min){
-                    System.out.println("available :: "+(min));
-                }
-                else{
-                    System.out.println("NOT AVAILABLE :: "+min);
-                    return min;
+        if (coins.length > 1) {
+            Arrays.sort(coins);
+            System.out.println("sorted : " + Arrays.toString(coins));
+
+            for (int coin : coins) {
+                if (coin > (min + 1)) {
+                    System.out.println("output : " + (min + 1));
+                    return min + 1;
+                } else {
+                    min = min + coin;
+                    System.out.println("coin : " + coin + "sum : " + min);
                 }
             }
         }
-
-
-        return min;
+        return 1;
     }
 }
