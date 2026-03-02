357485b015694787d51019866fe7088a7aaf0224
kishorkunal21 <kishorkunal21@gmail.com>
2026-03-02 08:00:33 +0530

    LinkedListCycles - what fast and slow pointers do

---
+++ src/com/educative/blind75/twopointers/fastandslowpointers/LinkedListCycles.java
@@ -0,0 +1,11 @@
+package com.educative.blind75.twopointers.fastandslowpointers;
+/*
+The two pointers pattern focuses on comparing data values,
+whereas the fast and slow pointers method is typically used to
+analyze the structure or properties of the data.
+
+Also known as Hare and Tortoise Algorithm - both will meet at some point in time in iteration
+Kind of problems where we need to find if a cycle or mid-point exists
+ */
+public class LinkedListCycles {
+}
